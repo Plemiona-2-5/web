bcc90feb54dc67df5e4556960579bcdecfc18492
krysduda <krystianduda64@gmail.com>
2021-04-29 14:22:25 +0200

    Filled needed fields, which will be shared

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    materials: {
+      wood: 0,
+      clay: 0,
+      iron: 0,
+      villagers: 0,
+    },
+    units: {
+      pikes: 0,
+      swords: 0,
+      axes: 0,
+      archers: 0,
+      trebuchets: 0,
+      horses: 0
+    },
+    village: {
+      x: 0,
+      y: 0,
+      points: 0
+    }
   },
   mutations: {
   },
