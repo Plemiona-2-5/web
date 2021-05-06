535ba179eee95297e60ea5bcbd702163d5a3a99b
krysduda <krystianduda64@gmail.com>
2021-05-06 10:18:03 +0200

    Installed SignalR and switched default port to 8086

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import VueSignalR from '@latelier/vue-signalr'
 
 Vue.config.productionTip = false
+Vue.use(VueSignalR, 'https://localhost:8080/')
 Vue.use(ElementUI);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
