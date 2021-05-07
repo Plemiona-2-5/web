5d6639270544bdf002e64e4f438401fb55a9c303
Bon3z <krystianduda64@gmail.com>
2021-05-07 13:13:38 +0200

    28 Create container for router view (#29)
    * 28 Create container for router view
    
    * Added basic styling to view container
    
    * Changed color
    
    * Moved styles to sass directory

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import VueSignalR from '@latelier/vue-signalr'
+import globalStyles from "./plugins/globalStyles";
 
 Vue.config.productionTip = false
 Vue.use(VueSignalR, 'https://localhost:8080/')
 Vue.use(ElementUI);
+Vue.use(globalStyles);
 
 new Vue({
     router,
