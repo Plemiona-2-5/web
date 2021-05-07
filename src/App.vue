89b87c7a930eb6d6190c4ae27aff388fd5d06e22
krysduda <krystianduda64@gmail.com>
2021-05-07 11:04:28 +0200

    Added basic styling to view container

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link>
+            |
+            <router-link to="/about">About</router-link>
+        </div>
+        <el-row :gutter="24" justify="center" align="top">
+            <el-col :offset="5" :span="14" id="view">
+                <router-view/>
+            </el-col>
+        </el-row>
     </div>
-    <el-row :gutter="24" justify="center" align="top">
-      <el-col :offset="6" :span="12">
-        <router-view/>
-      </el-col>
-    </el-row>
-  </div>
 </template>
 
 <style>
+html {
+    background-color: rgba(255, 211, 65, 0.4)
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
+}
+
+#view {
+    background-color: #42b983;
+    border: 5px outset rgba(255, 211, 65, 0.75);
 }
 </style>
