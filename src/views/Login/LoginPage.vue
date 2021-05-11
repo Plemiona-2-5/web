1ede6bfcf00193b80180b230cf726a171caba0f2
JW-39615 <kubawalega1@gmail.com>
2021-05-11 03:45:07 +0200

    Revert "created login form"
    This reverts commit 129db7f632c5b6f23066099f3e73ebb459a5adc0.

--- src/views/Login/LoginPage.vue
+++ src/views/Login/LoginPage.vue
@@ -1,15 +1,13 @@
 <template>
-	<div class="login-page">
-		<login-card></login-card>
-	</div>
+    
 </template>
 
 <script>
-import LoginCard from './content/LoginCard.vue';
+import { defineComponent } from '@vue/composition-api'
 
-export default {
-	components: {
-		LoginCard
-	}
-};
+export default defineComponent({
+    setup() {
+        
+    },
+})
 </script>
--- src/views/Login/content/LoginCard.vue
+++ src/views/Login/content/LoginCard.vue
@@ -1,53 +1,17 @@
 <template>
-	<div class="login">
-		<el-form ref="login-form" :model="form" :rules="rules" @submit.native.prevent>
-			<el-form-item prop="email" label="Email">
-				<el-input v-model="form.email" type="email"> </el-input>
-			</el-form-item>
-			<el-form-item prop="password" label="Password">
-				<el-input v-model="form.password" type="password"> </el-input>
-			</el-form-item>
-			<el-form-item>
-				<el-button type="primary" @click="LogIn" native-type="submit">Login</el-button>
-			</el-form-item>
-		</el-form>
-	</div>
+    
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			form: {
-				email: '',
-				password: ''
-			},
+import { defineComponent } from '@vue/composition-api'
 
-			rules: {
-				email: [
-					{
-						required: true,
-						message: 'Insert email address',
-						trigger: 'blur'
-					}
-				],
-				password: [
-					{
-						required: true,
-						message: 'Insert password',
-						trigger: 'blur'
-					}
-				]
-			}
-		};
-	},
-
-	methods: {
-		LogIn() {
-			console.log('login');
-		}
-	}
-};
+export default defineComponent({
+    setup() {
+        
+    },
+})
 </script>
 
-<style src="./LoginCard.css"></style>
+<style>
+
+</style>
