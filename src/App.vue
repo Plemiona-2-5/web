0e5205a195c0469414003fea9f4ee7221290be8c
krysduda <krystianduda64@gmail.com>
2021-05-13 10:14:08 +0200

    Finished style of navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
-    <div id="app">
-        <div id="nav">
-            <navbar></navbar>
-        </div>
+    <div>
+        <navbar></navbar>
         <el-row :gutter="24" justify="center" align="top">
             <el-col :offset="5" :span="14" id="view">
                 <router-view/>
@@ -10,24 +8,10 @@
         </el-row>
     </div>
 </template>
-
-<style>
-#nav {
-    padding: 30px;
-}
-
-#nav a {
-    font-weight: bold;
-    color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-    color: #42b983;
-}
-</style>
 <script>
-    import Navbar from "./components/Navbar";
-    export default {
-        components: {Navbar}
-    }
+import Navbar from "./components/Navbar";
+
+export default {
+    components: {Navbar}
+}
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,32 @@
 <template>
-    <div>
-        <router-link to="/">Home</router-link>
-        |
-        <router-link to="/about">About</router-link>
+    <div id="navbar">
+        <el-button type="danger" class="button">
+            <router-link to="/#">Overview</router-link>
+        </el-button>
+
+        <el-button type="danger" class="button">
+            <router-link to="/#">Map</router-link>
+        </el-button>
+
+        {{points}} Points
+
+        <el-button type="danger" class="button">
+            <router-link to="/#">Reports</router-link>
+        </el-button>
+
+        <el-button type="danger" class="button">
+            <router-link to="/#">Tribe</router-link>
+        </el-button>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Navbar"
+        name: "Navbar",
+        computed: {
+            points() {
+                return this.$store.getters.points
+            }
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
