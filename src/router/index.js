ac87df5c31dc037eef1871cb0fc406e3c12fe180
JW-39615 <kubawalega1@gmail.com>
2021-05-20 00:29:56 +0200

    Login page created

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,31 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 import OverviewPage from "../views/village/OverviewPage";
 import MapPage from "../views/map/MapPage";
+import LoginPage from "../views/login/LoginPage";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/',
-        name: 'OverviewPage',
-        component: OverviewPage
-    },
-    {
-        path: '/map',
-        name: 'MapPage',
-        component: MapPage
-    },
-]
+  {
+    path: "/",
+    name: "OverviewPage",
+    component: OverviewPage,
+  },
+  {
+    path: "/map",
+    name: "MapPage",
+    component: MapPage,
+  },
+  {
+    path: "/login",
+    name: "LoginPage",
+    component: LoginPage,
+  },
+];
 
 const router = new VueRouter({
-    routes
-})
+  routes,
+});
 
-export default router
+export default router;
