ce63b419845b6a15b0a32fabec68d5fa4df85848
krysduda <krystianduda64@gmail.com>
2021-05-21 14:53:12 +0200

    Drawn buildings on village display with placeholder images

---
+++ src/views/village/content/buildings.js
@@ -0,0 +1,26 @@
+class Building {
+    constructor(name, imageSrc, span = 8, offset = 0) {
+        this.name = name
+        this.imageSrc = imageSrc
+        this.span = span
+        this.offset = offset
+    }
+}
+
+let buildingsRows = {
+    firstRow: [
+        new Building("mine", require('@/assets/image/materials/wood.png'),),
+        new Building("farm", require('@/assets/image/materials/wood.png'), 8, 16),
+    ],
+    secondRow: [
+        new Building("granary", require('@/assets/image/materials/wood.png'),),
+        new Building("town hall", require('@/assets/image/materials/wood.png')),
+        new Building("barracks", require('@/assets/image/materials/wood.png')),
+    ],
+    thirdRow: [
+        new Building("brick yard", require('@/assets/image/materials/wood.png'), 4),
+        new Building("lumber mill", require('@/assets/image/materials/wood.png'), 4, 20),
+    ]
+}
+
+export default buildingsRows
