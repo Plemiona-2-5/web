92dae26896f40116453c7d6fa8e050d60f8de083
krysduda <krystianduda64@gmail.com>
2021-05-21 12:50:03 +0200

    Updated store

--- src/views/tribe/content/TribeCard.vue
+++ src/views/tribe/content/TribeCard.vue
@@ -1,24 +1,28 @@
 <template>
-    <el-row :gutter="18" justify="center">
+    <el-row :gutter="24" justify="center">
         <el-col :span="18" :offset="3">
-            <h1>{{tribeName}}</h1>
-            <h3>{{tribeDesc}}</h3>
+            <el-row :gutter="24">
+                <el-col :span="14">
+                    <h1>{{playerTribe.name}}</h1>
+                    <h4>{{playerTribe.points}} points</h4>
+                </el-col>
+                <el-col :span="10" id="desc">
+                    <h3>{{playerTribe.description}}</h3>
+                </el-col>
+            </el-row>
         </el-col>
     </el-row>
 </template>
 
 <script>
+    import {mapGetters} from "vuex"
+
     export default {
         name: "TribeCard",
-        data() {
-            return {
-                tribeName: "TribeNamePlaceholder",
-                tribeDesc: "TribeDescriptionPlaceholder"
-            }
+        computed: {
+            ...mapGetters([
+                "playerTribe"
+            ])
         }
     }
 </script>
-
-<style scoped>
-
-</style>
