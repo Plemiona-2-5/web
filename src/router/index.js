548cefd1c9faaaacbdcd17a5203f3e1c517aef43
krysduda <krystianduda64@gmail.com>
2021-05-21 13:22:50 +0200

    Added tribe browser

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import OverviewPage from "../views/village/OverviewPage";
 import MapPage from "../views/map/MapPage";
 import TribePage from "../views/tribe/TribePage";
+import TribeBrowser from "../views/tribe/content/TribeBrowser";
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
         path: '/tribe',
         name: 'TribePage',
         component: TribePage
+    },
+    {
+        path: '/tribe-browser',
+        name: 'TribeBrowser',
+        component: TribeBrowser
     },
 ]
 
