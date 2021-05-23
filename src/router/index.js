c24aa7bfa19bdf171a04eed1f773965a4065ef90
JW-39615 <kubawalega1@gmail.com>
2021-05-23 22:37:12 +0200

    home page created

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,31 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import OverviewPage from "../views/village/OverviewPage";
-import MapPage from "../views/map/MapPage";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import OverviewPage from '../views/village/OverviewPage';
+import MapPage from '../views/map/MapPage';
+import HomePage from '../views/home/HomePage';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/',
-        name: 'OverviewPage',
-        component: OverviewPage
-    },
-    {
-        path: '/map',
-        name: 'MapPage',
-        component: MapPage
-    },
-]
+	{
+		path: '/',
+		name: 'OverviewPage',
+		component: OverviewPage
+	},
+	{
+		path: '/map',
+		name: 'MapPage',
+		component: MapPage
+	},
+	{
+		path: '/home',
+		name: 'HomePage',
+		component: HomePage
+	}
+];
 
 const router = new VueRouter({
-    routes
-})
+	routes
+});
 
-export default router
+export default router;
