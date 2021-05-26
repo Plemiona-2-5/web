ce0746e8449c9cc8eef983570fe0b4d861e728ef
JW-39615 <kubawalega1@gmail.com>
2021-05-26 18:54:11 +0200

    initial card

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,37 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import OverviewPage from "../views/village/OverviewPage";
-import MapPage from "../views/map/MapPage";
-import LoginPage from "../views/login/LoginPage";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import OverviewPage from '../views/village/OverviewPage';
+import MapPage from '../views/map/MapPage';
+import LoginPage from '../views/login/LoginPage';
+import BarracksPage from '../views/barracks/BarracksPage';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "OverviewPage",
-    component: OverviewPage,
-  },
-  {
-    path: "/map",
-    name: "MapPage",
-    component: MapPage,
-  },
-  {
-    path: "/login",
-    name: "LoginPage",
-    component: LoginPage,
-  },
+	{
+		path: '/',
+		name: 'OverviewPage',
+		component: OverviewPage
+	},
+	{
+		path: '/map',
+		name: 'MapPage',
+		component: MapPage
+	},
+	{
+		path: '/login',
+		name: 'LoginPage',
+		component: LoginPage
+	},
+	{
+		path: '/barracks',
+		name: 'BarracksPage',
+		component: BarracksPage
+	}
 ];
 
 const router = new VueRouter({
-  routes,
+	routes
 });
 
 export default router;
---
+++ src/views/barracks/content/BarracksCard.vue
@@ -0,0 +1,71 @@
+<template>
+	<el-conteiner>
+		<div class="centre">
+			<el-header>
+				<materials-card> </materials-card>
+			</el-header>
+		</div>
+		<div class="body">
+			<el-main>
+				<div class="unit">
+					<h3>Archer</h3>
+					<el-image :src="archer_img"> </el-image>
+					<el-input-number v-model="archer" size="small" min="0"> </el-input-number>
+				</div>
+				<div class="unit">
+					<h3>Axeman</h3>
+					<el-image :src="axeman_image"> </el-image>
+					<el-input-number v-model="axeman" size="small" min="0"> </el-input-number>
+				</div>
+				<div class="unit">
+					<h3>Swordsman</h3>
+					<el-image :src="swordsman_image"> </el-image>
+					<el-input-number v-model="swordsman" size="small" min="0"> </el-input-number>
+				</div>
+				<div class="unit">
+					<h3>Horseman</h3>
+					<el-image :src="horseman_image"> </el-image>
+					<el-input-number v-model="horseman" size="small" min="0"> </el-input-number>
+				</div>
+				<div class="unit">
+					<h3>Heavy horseman</h3>
+					<el-image :src="h_horseman_image"> </el-image>
+					<el-input-number v-model="h_horseman" size="small" min="0"> </el-input-number>
+				</div>
+				<div class="confirm">
+					<el-button type="primary">Add to recruitment queue</el-button>
+				</div>
+			</el-main>
+		</div>
+	</el-conteiner>
+</template>
+
+<script>
+import MaterialsCard from '../../village/content/MaterialsCard.vue';
+
+const archer_img = require('@/assets/image/units/pic.png');
+const axeman_image = require('@/assets/image/units/axe.png');
+const swordsman_image = require('@/assets/image/units/sword.png');
+const horseman_image = require('@/assets/image/units/light.png');
+const h_horseman_image = require('@/assets/image/units/heavy.png');
+
+export default {
+	name: 'BarracksCard',
+	components: { MaterialsCard },
+	data() {
+		return {
+			archer_img,
+			axeman_image,
+			swordsman_image,
+			horseman_image,
+			h_horseman_image,
+			archer: 0,
+			axeman: 0,
+			swordsman: 0,
+			horseman: 0,
+			h_horseman: 0
+		};
+	},
+	methods: {}
+};
+</script>
