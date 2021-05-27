15ab765a3620ed80bf32ed30711d099b028232dc
JW-39615 <kubawalega1@gmail.com>
2021-05-27 11:31:46 +0200

    redesign

--- src/views/barracks/content/BarracksCard.vue
+++ src/views/barracks/content/BarracksCard.vue
@@ -1,49 +1,58 @@
 <template>
-	<el-conteiner>
+	<div>
 		<div class="centre">
 			<el-header>
 				<materials-card> </materials-card>
 			</el-header>
 		</div>
-		<div class="body">
-			<el-main>
+		<el-col :span="3">
+			<el-row v-for="(state, name) in infantry" :key="name">
+				<h3>{{ name }}</h3>
+			</el-row>
+			<el-row v-for="(state, name) in cavalry" :key="name">
+				<h3>{{ name }}</h3>
+			</el-row>
+		</el-col>
+		<el-col :span="6">
+			<el-row>
 				<div class="unit">
-					<h3>Archer</h3>
-					<el-image :src="archer_img"> </el-image>
-					<el-input-number v-model="archer" size="small" min="0"> </el-input-number>
+					<el-image :src="pikeman_img"> </el-image>
+					<el-input-number v-model="pikeman" size="small" :min="0"> </el-input-number>
 				</div>
 				<div class="unit">
-					<h3>Axeman</h3>
-					<el-image :src="axeman_image"> </el-image>
-					<el-input-number v-model="axeman" size="small" min="0"> </el-input-number>
-				</div>
-				<div class="unit">
-					<h3>Swordsman</h3>
 					<el-image :src="swordsman_image"> </el-image>
-					<el-input-number v-model="swordsman" size="small" min="0"> </el-input-number>
+					<el-input-number v-model="swordsman" size="small" :min="0"> </el-input-number>
 				</div>
 				<div class="unit">
-					<h3>Horseman</h3>
+					<el-image :src="axeman_image"> </el-image>
+					<el-input-number v-model="axeman" size="small" :min="0"> </el-input-number>
+				</div>
+				<div class="unit">
 					<el-image :src="horseman_image"> </el-image>
-					<el-input-number v-model="horseman" size="small" min="0"> </el-input-number>
+					<el-input-number v-model="horseman" size="small" :min="0"> </el-input-number>
 				</div>
 				<div class="unit">
-					<h3>Heavy horseman</h3>
 					<el-image :src="h_horseman_image"> </el-image>
-					<el-input-number v-model="h_horseman" size="small" min="0"> </el-input-number>
+					<el-input-number v-model="h_horseman" size="small" :min="0"> </el-input-number>
 				</div>
+			</el-row>
+		</el-col>
+		<el-col>
+			<el-row>
 				<div class="confirm">
-					<el-button type="primary">Add to recruitment queue</el-button>
+					<el-button type="primary" @click="Recruit()">Add to recruitment queue</el-button>
 				</div>
-			</el-main>
-		</div>
-	</el-conteiner>
+			</el-row>
+		</el-col>
+	</div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import { units } from '@/store/unitsModule';
 import MaterialsCard from '../../village/content/MaterialsCard.vue';
 
-const archer_img = require('@/assets/image/units/pic.png');
+const pikeman_img = require('@/assets/image/units/pic.png');
 const axeman_image = require('@/assets/image/units/axe.png');
 const swordsman_image = require('@/assets/image/units/sword.png');
 const horseman_image = require('@/assets/image/units/light.png');
@@ -54,18 +63,26 @@ export default {
 	components: { MaterialsCard },
 	data() {
 		return {
-			archer_img,
+			units,
+			pikeman_img,
 			axeman_image,
 			swordsman_image,
 			horseman_image,
 			h_horseman_image,
-			archer: 0,
+			pikeman: 0,
 			axeman: 0,
 			swordsman: 0,
 			horseman: 0,
 			h_horseman: 0
 		};
 	},
-	methods: {}
+	computed: {
+		...mapGetters(['infantry', 'cavalry'])
+	},
+	methods: {
+		Recruit() {
+			console.log('call recruitment');
+		}
+	}
 };
 </script>
