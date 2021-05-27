8a24e66f9e7d08fe9767ae4853f6e575352d17ba
krysduda <krystianduda64@gmail.com>
2021-05-27 14:04:33 +0200

    52-create-logic-for-logging-in

--- src/views/login/content/LoginCard.vue
+++ src/views/login/content/LoginCard.vue
@@ -1,53 +1,58 @@
 <template>
-  <el-form
-    :model="loginForm"
-    :rules="rules"
-    ref="LoginForm"
-    label-width="120px"
-  >
-    <el-form-item label="Email" prop="email">
-      <el-input v-model="loginForm.email"> </el-input>
-    </el-form-item>
-    <el-form-item label="Password" prop="password">
-      <el-input v-model="loginForm.password"> </el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button id="loginButton" type="primary" @click="Login()">
-        Login
-      </el-button>
-    </el-form-item>
-  </el-form>
+    <el-form
+            :model="loginForm"
+            :rules="rules"
+            ref="LoginForm"
+            label-width="120px"
+    >
+        <el-form-item label="Email" prop="email">
+            <el-input v-model="loginForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="Password" prop="password">
+            <el-input v-model="loginForm.password" type="password"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-alert type="error" v-model="errors" v-for="errorType in errors" :key="errorType">
+                <li v-for="error in errorType" :key="error">{{error}}</li>
+            </el-alert>
+            <el-button id="loginButton" type="primary" @click="login()">
+                Login
+            </el-button>
+        </el-form-item>
+    </el-form>
 </template>
 
 <script>
-import { email, password } from "./validationRules.js";
+    import {email, password} from "./validationRules.js";
+    import axios from "axios";
 
-export default {
-  data() {
-    return {
-      loginForm: {
-        email: "",
-        password: "",
-      },
-      rules: {
-        email,
-        password,
-      },
+    export default {
+        data() {
+            return {
+                loginForm: {
+                    email: "",
+                    password: "",
+                },
+                rules: {
+                    email,
+                    password,
+                },
+                errors: []
+            };
+        },
+        methods: {
+            login() {
+                this.$refs.LoginForm.validate((valid) => {
+                    if (valid) {
+                        axios.post("api/auth/login", {...this.loginForm}).then(
+                            (response) => console.log("gut", response),
+                            (error) => this.errors = error.response.data.errors
+                        )
+                    }
+                });
+            },
+        },
     };
-  },
-  methods: {
-    Login() {
-      this.$refs.LoginForm.validate((valid) => {
-        if (valid) {
-          alert("submit");
-        } else {
-          console.log("error");
-          return false;
-        }
-      });
-    },
-  },
-};
 </script>
 
 <style scoped></style>
